d3771610c446382d1d86e6c193e99057a4d8c61b
Saad Abdullah <saadabdullah314405@gmail.com>
2025-09-30 19:26:31 +0500

    fix

--- app/components/ShopComponents/CollectionsSection.js
+++ app/components/ShopComponents/CollectionsSection.js
@@ -248,8 +248,7 @@ const CollectionsSection = ({ collections, collectionsLoading, collectionsError 
           <div className="text-center">
             <button
               onClick={handleLoadMore}
-              disabled={isLoading}
-              className="inline-flex items-center px-6 py-3 bg-[#8bc34a] text-white font-bold rounded-full transition-all duration-300 transform  hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-gray-600 focus:ring-opacity-50 disabled:opacity-50 disabled:cursor-not-allowed"
+              className="inline-flex items-center px-6 py-3 bg-gradient-to-r from-gray-800 to-black hover:from-black hover:to-black text-white font-bold rounded-full transition-all duration-300 transform hover:shadow-xl focus:outline-none focus:ring-4 focus:ring-gray-600 focus:ring-opacity-50"
             >
               Load More
             </button>
