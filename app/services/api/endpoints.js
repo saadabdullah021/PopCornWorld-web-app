c0badaaae78f5a07266a12f4331e5cdf133d757c
Mubeen Hussain <mubeenhussain8@gmail.com>
2025-10-04 20:31:15 +0500

    flavor detail page

--- app/services/api/endpoints.js
+++ app/services/api/endpoints.js
@@ -2,3 +2,4 @@ export const get_global_settings = "/api/get-global-settings";
 export const get_campaigns = "/api/get-campaigns";
 export const get_products = "/api/get-products";
 export const get_shop_collections = "/api/get-collections";
+export const get_single_product = "/api/get-product";
--- app/services/api/index.js
+++ app/services/api/index.js
@@ -1,4 +1,5 @@
 import axiosInstance from '../../axios'
+import { get_single_product } from './endpoints'
 
 export const api = {
   get: (url, config = {}) => axiosInstance.get(url, config),
@@ -7,5 +8,19 @@ export const api = {
   patch: (url, data = {}, config = {}) => axiosInstance.patch(url, data, config),
   delete: (url, config = {}) => axiosInstance.delete(url, config),
 }
+
+export const getSingleProduct = async (slug, success, fail) => {
+  try {
+    const response = await axiosInstance.get(`${get_single_product}?slug=${slug}`);
+    if (response?.data?.status === 200) {
+      let data = response?.data?.data;
+      success && success(data);
+      return response?.data?.data;
+    }
+  } catch (error) {
+    fail && fail();
+    return error;
+  }
+};
 
 export default axiosInstance
